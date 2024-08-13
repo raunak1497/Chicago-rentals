533f3b66424fcc235c24c3138a0eedb12780804f
RAUNAK SINGH <raunak1497@gmail.com>
2024-08-12 21:00:27 -0500

    First commit

---
+++ ChicagoRentals/src/main/java/com/uic/chicagorentals/ChicagoRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.uic.chicagorentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicagoRentalsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChicagoRentalsApplication.class, args);
+    }
+
+}
---
+++ ChicagoRentals/src/test/java/com/uic/chicagorentals/ChicagoRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uic.chicagorentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicagoRentalsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
