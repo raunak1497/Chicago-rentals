f5b349d49d69d8bb247b7217ee7841b5d523819d
RAUNAK SINGH <raunak1497@gmail.com>
2024-08-19 23:19:04 -0500

    Implemented CRUD APIs

---
+++ booking-service/src/main/java/com/uic/bookingservice/collection/Status.java
@@ -0,0 +1,7 @@
+package com.uic.bookingservice.collection;
+
+public enum Status {
+    BOOKED,
+    SHIPPED,
+    OUT_FOR_PICKUP
+}
---
+++ booking-service/src/main/java/com/uic/bookingservice/service/BookingService.java
@@ -0,0 +1,19 @@
+package com.uic.bookingservice.service;
+
+import com.uic.bookingservice.collection.Booking;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+public interface BookingService {
+    Booking createBooking(Booking booking);
+
+    List<Booking> getAllBooking();
+
+    Booking getBookingById(String id);
+
+    void deleteBooking(String id);
+
+    void updateBooking(Booking booking, String id);
+}
